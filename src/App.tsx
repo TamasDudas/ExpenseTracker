138f42e67ed0abbb6d3719c3ca1793d13519aa72
Dudás Tamás <ddstms@gmail.com>
2026-03-14 14:02:58 +0100

    feat: add filtering functionality to expense list with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import reducer from './reducer';
 import { useEffect, useMemo, useReducer, useState } from 'react';
 import type { Expense } from './types/type';
 import ExpenseList from './components/ExpenseList';
+import { Button } from './components/ui/button';
 
 function App() {
  const [editingExpense, setEditingExpense] = useState<Expense | null>(null);
+ const [filtered, setFiltered] = useState<'all' | 'income' | 'expense'>('all');
 
  const [expenses, dispatch] = useReducer(
   reducer,
@@ -31,6 +33,11 @@ function App() {
 
  const total = totalIncome - totalExpense;
 
+ const filteredExpenses = useMemo(() => {
+  if (filtered === 'all') return expenses;
+  return expenses.filter((expense) => expense.type === filtered);
+ }, [expenses, filtered]);
+
  useEffect(() => {
   localStorage.setItem('expenses', JSON.stringify(expenses));
  }, [expenses]);
@@ -52,23 +59,41 @@ function App() {
   setEditingExpense(null);
  };
 
- console.log('Total:', total);
- console.log('Total Expense:', totalExpense);
- console.log('Total Income:', totalIncome);
-
+ const getVariant = (value: 'all' | 'income' | 'expense') => {
+  return filtered === value ? 'default' : 'outline';
+ };
  return (
   <div className="max-w-7xl mx-auto p-8">
-   <h1 className="text-3xl font-bold underline">Expense Tracker</h1>
+   <h1 className="text-3xl font-bold underline my-12">Expense Tracker</h1>
+
    <ExpenseForm
     onAdd={handleAddExpense}
     editingExpense={editingExpense}
     onUpdate={handleUpdate}
    />
-   <ExpenseList
-    expenses={expenses}
-    onDelete={handleDelete}
-    onEdit={handleEdit}
-   />
+   <div className="max-w-xl mx-auto ">
+    <Button variant={getVariant('all')} onClick={() => setFiltered('all')}>
+     All
+    </Button>
+    <Button
+     variant={getVariant('income')}
+     onClick={() => setFiltered('income')}
+    >
+     Income
+    </Button>
+    <Button
+     variant={getVariant('expense')}
+     onClick={() => setFiltered('expense')}
+    >
+     Expense
+    </Button>
+
+    <ExpenseList
+     expenses={filteredExpenses}
+     onDelete={handleDelete}
+     onEdit={handleEdit}
+    />
+   </div>
   </div>
  );
 }
